1ef10cd026f09af6f3547e4f5922a4420283ad97
unknown <cev1488@innovaccion.mx>
2022-04-21 00:51:15 -0500

    correccion html

--- app/app.py
+++ app/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+#from flask_bootstrap import Bootstrap
 app = Flask(__name__)
-Bootstrap(app)
+# Bootstrap(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/top')
+def top():
+    return render_template('top.html')
 
-if __name__=='__main__':
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
