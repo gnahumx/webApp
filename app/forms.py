4ca0b9aab4e86c4cc41b308ef20eeb27825d5158
unknown <cev1488@innovaccion.mx>
2022-04-24 01:12:28 -0500

    formula

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, Email, Length
 class Persona(FlaskForm):
     nombre = StringField("Nombre", validators=[DataRequired( message="El campo es obligatorio."), Length(max=64, min=4, message="El campo debe tener entre 4 y 10 caracteres.")])
     genero = StringField("Genero", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired("El campo es obligatorio."), Email( message="Introduzca una dirección de correo válida.")])
     #password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
 
